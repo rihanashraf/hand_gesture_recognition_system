1c02d97ec097f21c88b1f3b12aab6eb0a79b22e0
rihanashraf <rihanthachu@gmail.com>
2026-08-17 16:24:56 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""
+Model Package
+Author: Your Name
+"""
+from model.keypoint_classifier.keypoint_classifier import KeyPointClassifier
+from model.point_history_classifier.point_history_classifier import PointHistoryClassifier
