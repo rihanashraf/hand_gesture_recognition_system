b62a9e2634158207770c6a638041c4c31eb70069
rihanashraf <rihanthachu@gmail.com>
2026-08-17 16:25:11 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 """
-Model Package
+Utils Package
 Author: Your Name
 """
-from model.keypoint_classifier.keypoint_classifier import KeyPointClassifier
-from model.point_history_classifier.point_history_classifier import PointHistoryClassifier
+from utils.cvfpscalc import CvFpsCalc
